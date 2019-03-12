03ae1c40113fefb0049643b3aa09d9566ec5780d
Kyle Strand <KYLE.STRAND@BECKMAN.COM>
2019-03-12 17:02:53 -0600

    implement most of 'attack'

--- sim/src/actions/attack.rs
+++ sim/src/actions/attack.rs
@@ -14,32 +14,28 @@ struct DeclaredAttack {
     def_power: PowerType
 }
 
-// Initiates an attack, returning a closure over the data necessary to perform the next step of the
-// attack.
-fn declare_attack(attacker: &str,
-                 defender: &str,
-                 def_power: PowerType)
-                 // Note: could use unstable feature:
-                 // http://www.integer32.com/2017/02/02/stupid-tricks-with-higher-order-functions.html
-                 -> AddingDefendersResult {
+impl DeclaredAttack {
+    // Initiates an attack, returning a closure over the data necessary to perform the next step of the
+    // attack.
+    fn declare(attacker: &str,
+                    defender: &str,
+                    def_power: PowerType)
+                    // Note: could use unstable feature:
+                    // http://www.integer32.com/2017/02/02/stupid-tricks-with-higher-order-functions.html
+                    -> AddDefender {
 
-    let attack = DeclaredAttack {
-        attackers: vec![String::from(attacker)],
-        defenders: vec![String::from(defender)],
-        def_power: def_power };
+        let attack = DeclaredAttack {
+            attackers: vec![String::from(attacker)],
+            defenders: vec![String::from(defender)],
+            def_power: def_power };
 
-    // Q: Any way to infer the inner `AddDefender` type?
-    AddingDefendersResult::AddDefender(AddDefender{attack: attack})
-}
+        // Q: Any way to infer the inner `AddDefender` type?
+        AddDefender{attack}
+    }
 
-enum AddingDefendersResult {
-    AddDefender(AddDefender),
-    AddAttacker(AddAttacker)
-}
-
-enum AddingAttackersResult {
-    AddAttacker(AddAttacker),
-    Outcome(Outcome)
+    fn finalize(self) -> Outcome {
+        unimplemented!();
+    }
 }
 
 struct AddDefender {
@@ -47,10 +43,8 @@ struct AddDefender {
 }
 
 impl AddDefender {
-    fn add(self, name: &str) -> AddingDefendersResult {
-        // Q: Why no automatic wrapping as `Some`?
-        // Maybe add a `derive` to impl deref as `Some`?
-        add_defender_impl(self.attack, Some(name))
+    fn add(&mut self, name: &str) {
+        self.attack.defenders.push(name.to_owned());
     }
     fn finalize_defense(self) -> AddAttacker {
         AddAttacker { attack: self.attack }
@@ -62,16 +56,10 @@ struct AddAttacker {
 }
 
 impl AddAttacker {
-    fn add(self, name: &str) { add_attacker_impl(self.attack, Some(name)); }
-    fn finalize_offense(self) -> Outcome {
-        Outcome { /* XXX TODO */ }
+    fn add(&mut self, name: &str) { 
+        self.attack.attackers.push(name.to_owned());
     }
-}
-
-fn add_defender_impl(_attack: DeclaredAttack, _co_defender: Option<&str>) -> AddingDefendersResult {
-    unimplemented!();
-}
-
-fn add_attacker_impl(_attack: DeclaredAttack, _co_attacker: Option<&str>) -> AddingAttackersResult {
-    unimplemented!();
+    fn finalize_offense(self) -> Outcome {
+        self.attack.finalize()
+    }
 }
